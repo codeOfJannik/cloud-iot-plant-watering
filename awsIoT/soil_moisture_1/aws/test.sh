ed9c6747178563d2e615a7fc0d40686536d3419f
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-05-19 11:05:09 +0200

    changed .gitignore and removed test.sh from remote repo

--- awsIoT/soil_moisture_1/aws/test.sh
+++
@@ -1,3 +0,0 @@
-# run pub/sub sample app using certificates downloaded in package
-printf "\nRunning pub/sub sample application...\n"
-python aws-iot-device-sdk-python/samples/basicPubSub/basicPubSub.py -e a1f7wlp3u17m26-ats.iot.us-east-1.amazonaws.com -r root-CA.crt -c soil_moisture_1.cert.pem -k soil_moisture_1.private.key
