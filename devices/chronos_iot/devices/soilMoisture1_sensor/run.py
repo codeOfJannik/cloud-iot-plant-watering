fb1a3794a3cf9a0cda98d23da6314601e34750d6
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-06-24 17:46:58 +0200

    removed additional publish of soil moisture sensors

--- devices/chronos_iot/devices/soilMoisture1_sensor/run.py
+++ devices/chronos_iot/devices/soilMoisture1_sensor/run.py
@@ -1,9 +1,7 @@
 from software_class.device_software import DeviceSoftware
-from software_class.policy_topic_reader import *
 
-topic = shorten_topic(get_topic(read_policy("./policy.json")))
 device_software = DeviceSoftware()
-device_software.run_update_data(topic)
+device_software.run_update_data()
 
 
 if __name__ == "__main__":
