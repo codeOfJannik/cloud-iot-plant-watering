96bd9eb17fee34fdfcfd6c25c88f672f1025f92c
Patrick-Eichhorn <eichhorn.gp@gmail.com>
2020-06-30 12:04:18 +0200

    add core rule policy

--- devices/chronos_iot/terraform/coreRule.tf
+++ devices/chronos_iot/terraform/coreRule.tf
@@ -7,7 +7,7 @@ resource "aws_iot_topic_rule" "control_panel_rule" {
 
   iot_events {
     input_name = "ControlPanelInput"
-    role_arn   = "arn:aws:iam::002917872344:role/service-role/soilMoisture_core_rule_role" //aws_iam_role.controlPanel_core_rule_role_copy.arn
+    role_arn   = aws_iam_role.core_rule_role.arn
   }
 }
 
@@ -20,7 +20,7 @@ resource "aws_iot_topic_rule" "soil_moisture_rule" {
 
   iot_events {
     input_name = "SoilMoistureInput"
-    role_arn   = "arn:aws:iam::002917872344:role/service-role/soilMoisture_core_rule_role" //aws_iam_role.controlPanel_core_rule_role_copy.arn
+    role_arn   = aws_iam_role.core_rule_role.arn
   }
 }
 
@@ -33,42 +33,64 @@ resource "aws_iot_topic_rule" "rain_barrel_rule" {
 
   iot_events {
     input_name = "RainBarrelSensorInput"
-    role_arn   = "arn:aws:iam::002917872344:role/service-role/rainBarrel_core_rule_role" //aws_iam_role.controlPanel_core_rule_role_copy.arn
+    role_arn   = aws_iam_role.core_rule_role.arn
   }
 }
 
-//data "aws_iam_policy_document" "core_rule_permissions" {
-//  statement {
-//    effect = "Allow"
-//    actions = [
-//      "iotevents:BatchPutMessage"
-//      ]
-//    resources = [
-//      "arn:aws:iotevents:us-east-1:002917872344:input/SoilMoistureInput"
-//    ]
-//  }
-//  statement {
-//    effect = "Allow"
-//    actions = [
-//      "iotevents:BatchPutMessage"
-//    ]
-//    resources = [
-//      "arn:aws:iotevents:us-east-1:002917872344:input/ControlPanelInput"
-//    ]
-//  }
-//  statement {
-//    effect = "Allow"
-//    actions = [
-//      "iot:Publish"
-//    ]
-//    resources = [
-//      "arn:aws:iot:us-east-1:002917872344:topic/events/rules/test"
-//    ]
-//  }
-//}
-//
-//resource "aws_iam_role" "controlPanel_core_rule_role_copy" {
-//  name               = "controlPanel_core_rule_role_copy"
-//  path               = "/service-role/"
-//  assume_role_policy = data.aws_iam_policy_document.core_rule_permissions.json
-//}
+resource "aws_iam_policy" "core_rule_policy" {
+  name = "test_policy"
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "ec2:Describe*"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": "iotevents:BatchPutMessage",
+        "Resource": [
+            "arn:aws:iotevents:us-east-1:002917872344:input/SoilMoistureInput",
+            "arn:aws:iotevents:us-east-1:002917872344:input/ControlPanelInput",
+            arn:aws:iotevents:us-east-1:002917872344:input/RainBarrelSensorInpu
+        ]
+      },
+      {
+        "Effect": "Allow",
+        "Action": "iot:Publish",
+        "Resource": "arn:aws:iot:us-east-1:002917872344:topic/events/rules/test"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_role_policy" {
+  policy_arn = aws_iam_role_policy.core_rule_policy.arn
+  role = aws_iam_role.core_rule_role.name
+}
+
+resource "aws_iam_role" "core_rule_role" {
+  name = "core_rule_role"
+  path = "/service-role/"
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "AWS": "arn:aws:iam::002917872344:role/*"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
