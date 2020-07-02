31bd537ce6dc663aa720ca25801fd11f1bef8362
Patrick-Eichhorn <eichhorn.gp@gmail.com>
2020-07-02 08:54:05 +0200

    change order

--- iot_core/chronos_infrastructure/main.tf
+++ iot_core/chronos_infrastructure/main.tf
@@ -3,10 +3,8 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-module "docker" {
-  source = "./docker"
-
-  dependencies = [module.iot_core.docker_dependencies]
+module "iot_events" {
+  source = "./iot_events"
 }
 
 module "iot_core" {
@@ -15,6 +13,8 @@ module "iot_core" {
   dependencies = [module.iot_events.iot_core_dependencies]
 }
 
-module "iot_events" {
-  source = "./iot_events"
+module "docker" {
+  source = "./docker"
+
+  dependencies = [module.iot_core.docker_dependencies]
 }
