cae106fb310f258a01a428af815b96746fb4e5d1
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-07-09 16:45:50 +0200

    added in-code documentation of aws_iot_client.py

--- iot_core/software_class/aws_iot_client.py
+++ iot_core/software_class/aws_iot_client.py
@@ -5,42 +5,36 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 class AWSIoTClient:
     def __init__(self, root_ca_path, certificate_path, private_key_path, device_name):
         """
-        TODO
-        :param root_ca_path: TODO
-        :param certificate_path: TODO
-        :param private_key_path: TODO
-        :param device_name: TODO
+        Initializes AWSIoTClient class using the AWSIoTMQTTClient class provided by AWS in the
+        aws-iot-device-sdk-python. The MQTT client is configured with the endpoint address of the AWS account and
+        the credential files for the specific device need to be passed to the MQTT client.
+        :param root_ca_path: path to the AWS root-CA downloaded in each device docker container (Dockerfile)
+        :param certificate_path: path to the AWS IoT device specific certificate file created during terraform setup
+        :param private_key_path: path to the AWS IoT device specific private key file created during terraform setup
+        :param device_name: name of the AWS IoT device
         """
         self.mqttClient = AWSIoTMQTTClient(device_name)
         self.mqttClient.configureEndpoint(os.getenv("AWS_IOT_ENDPOINT"), 8883)
         self.mqttClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
 
-        # TODO: check configuration options
-        # AWSIoTMQTTClient connection configuration
-        # self.mqttClient.configureAutoReconnectBackoffTime(1, 32, 20)
-        # self.mqttClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-        # self.mqttClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-        # self.mqttClient.configureConnectDisconnectTimeout(10)  # 10 sec
-        # self.mqttClient.configureMQTTOperationTimeout(5)  # 5 sec
-
         self.mqttClient.connect()
 
     def subscribe_to_topic(self, topic, custom_callback, quality_of_service) -> bool:
         """
-        TODO
-        :param topic: TODO
-        :param custom_callback: TODO
-        :param quality_of_service: TODO
-        :return: [bool]
+        Client tries to subscribe to a defined MQTT topic, and registers callback for incoming messages on the topic.
+        :param topic: the MQTT topic the client should subscribe to
+        :param custom_callback: the callback that should be executed when messages on the topic are received
+        :param quality_of_service: qos of the subscription (1 = confirm received message, 0 = no confirmation)
+        :return: True if the subscribe attempt succeeded. False if failed.
         """
         return self.mqttClient.subscribe(topic, quality_of_service, custom_callback)
 
     def publish_message_to_topic(self, message, topic, quality_of_service) -> bool:
         """
-        TODO
-        :param message: TODO
-        :param topic: TODO
-        :param quality_of_service: TODO
+        Client tries to published a message in a defined MQTT topic.
+        :param message: json message to be published
+        :param topic: topic where the message should be published
+        :param quality_of_service: 1 if confirmation of received message required ,0 if no confirmation required
         :return: [bool]
         """
         return self.mqttClient.publish(topic, message, quality_of_service)
