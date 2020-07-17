f8a71aa92dd6f1f13fd4df8a3d6ac76d2984dc2e
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-07-18 00:06:33 +0200

    removed unused imports

--- tests/testcase_creation.py
+++ tests/testcase_creation.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, main
-from unittest.mock import patch, PropertyMock, Mock
+from unittest.mock import patch
 from iot_core.software_class.device_software import DeviceSoftware
 
 
